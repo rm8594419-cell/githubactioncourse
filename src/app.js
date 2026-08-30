1cdc2207453b84108688f508cecdf9e9222cbce0
Super User <root@localhost.localdomain>
2026-08-30 09:11:13 -0400

    adds the test workfiowv

---
+++ src/app.js
@@ -0,0 +1,10 @@
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+
+module.exports = greet;
+
+// If run directly, print output (useful for debugging)
+if (require.main === module) {
+    console.log(greet("World"));
+}
