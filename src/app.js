9810a044479e2e85c20c12060e92780a887494f9
Super User <root@localhost.localdomain>
2026-08-30 09:35:12 -0400

    test changes

--- src/app.js
+++ src/app.js
@@ -5,6 +5,6 @@ function greet(name) {
 module.exports = greet;
 
 // If run directly, print output (useful for debugging)
-if (require.main === module) {
-    console.log(greet("World"));
-}
+//if (require.main === module) {
+    //console.log(greet("World"));
+//}
